18d68e516185511f2102e2afcf7e46f4692f46c2
LiarleyCodie <liarley.personal@gmail.com>
2022-07-30 17:59:18 -0300

    :tada: animações suavizadas pra quando o menu for aberto e fechado

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,29 @@
 const themesButton = document.getElementById("themesButton");
 const themesMenu = document.getElementById("themesMenu");
 
-themesButton.addEventListener("click", function() {
-		themesMenu.classList.toggle("active");
+window.addEventListener("click", ({ target }) => {
+	if (target === themesButton) {
+		if (!themesMenu.classList.contains('active')) {
+			themesMenu.style.animation = "showMenu 300ms ease backwards"
+			themesMenu.classList.add("active")
+		} else {
+			themesMenu.style.animation = "hiddenMenu 300ms ease backwards"
+
+			themesMenu.addEventListener("animationend", ({ animationName }) => {
+				if (animationName === 'hiddenMenu') {
+						themesMenu.classList.remove("active")
+				}
+			})
+		}
+	}
+
+	if (target !== themesButton) {
+		themesMenu.style.animation = "hiddenMenu 400ms ease backwards"
+
+		themesMenu.addEventListener("animationend", ({ animationName }) => {
+			if (animationName === 'hiddenMenu') {
+					themesMenu.classList.remove("active")
+			}
+		})
+	}
 })
